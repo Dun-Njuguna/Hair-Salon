d0b0de229bb591047bb06b28e37b18282d2a1eae
Dun-Njuguna <duncankariuki60@gmail.com>
2019-02-07 14:29:40 +0300

    added an override method for the client class

--- src/main/java/Clients.java
+++ src/main/java/Clients.java
@@ -45,17 +45,17 @@ public class Clients {
     }
 
 
-      // @Override
-      // public boolean equals(Object otherClient){
-      //   if (!(otherClient instanceof Clients)) {
-      //     return false;
-      //   } else {
-      //     Clients newClient = (Clients) otherClient;
-      //     return this.getName().equals(newClient.getName()) &&
-      //            this.getId() == newClient.getId() &&
-      //            this.getStylistId() == newClient.getStylistId();
-      //   }
-      // }
+      @Override
+      public boolean equals(Object otherClient){
+        if (!(otherClient instanceof Clients)) {
+          return false;
+        } else {
+          Clients newClient = (Clients) otherClient;
+          return this.getName().equals(newClient.getName()) &&
+                 this.getId() == newClient.getId() &&
+                 this.getStylistId() == newClient.getStylistId();
+        }
+      }
 
 
       // public void save() {
